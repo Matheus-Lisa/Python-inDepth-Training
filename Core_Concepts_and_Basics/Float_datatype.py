8f62e87149efe9f839ff2fbc22016191d0bdec67
Mhatayur <matheus_codework@protonmail.com>
2025-02-18 20:42:47 -0300

    Adding Float Datatype

---
+++ Core_Concepts_and_Basics/Float_datatype.py
@@ -0,0 +1,5 @@
+#A floating-point number (or simply "float") is a numerical data type used in Python to represent real numbers that have a fractional part. 
+#They are used when precision with decimal values is needed.
+#In Python, floating-point numbers are implemented using the double-precision (64-bit) IEEE 754 standard. 
+# This format allows for a wide range of values but comes with certain limitations due to the way numbers are stored in memory.
+
